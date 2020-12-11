11289f34bee3ecf229f191eed9f429de52a90d27
Hamid Kianzad <hamid.kianzad@suzohapp.com>
2020-12-11 16:34:58 +0100

    Whiteboard pattern

---
+++ java-design-patterns-behavioral/java-design-patterns-behavioral-whiteboard/src/main/java/com/onixys/learning/dpb/whiteboard/Main.java
@@ -0,0 +1,14 @@
+package com.onixys.learning.dpb.whiteboard;
+
+/**
+ * Main
+ *
+ * @author Hamid Kianzad
+ * @version {version}
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
